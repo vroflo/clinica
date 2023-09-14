bf65e419a1c875b31a4b9a35103cdc89f095d708
sol <solmonlezun@gmail.com>
2023-09-13 21:18:25 -0300

    cree un prompt de ingreso y alert de bienvnida

---
+++ clinica.js
@@ -0,0 +1,14 @@
+let registrar = prompt("ingreses su nombre completo para ser registrado: nombre y apellido");
+alert("Usted fue ingresado con exito, Bienvenido a nuestra clinica!");
+
+
+
+
+
+
+
+
+
+
+
+
